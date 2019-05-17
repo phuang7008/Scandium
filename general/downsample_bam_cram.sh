a9b65913ff92a62d83b6aa2dc1f4bf20396315e9
phuang <phuang@bcm.edu>
2019-05-17 09:19:27 -0500

    before modify the downsample_bam_cram.sh to latest picard (2.20.1)

--- general/downsample_bam_cram.sh
+++ general/downsample_bam_cram.sh
@@ -22,9 +22,9 @@ fi;
 ##Check to see if the input file is a bam file
 if [ ${IN_FILE: -4} == ".bam" ]; then
 	#${JAVA} -Xmx15G -jar /hgsc_software/picard/picard-tools-1.54/DownsampleSam.jar I=${IN_FILE} O=${IN_FILE}_downsampled_${REDUCERATE}.bam P=${REDUCERATE};
-	${JAVA} -Xmx32G -jar /hgsc_software/picard/picard-tools-2.6.0/picard.jar DownsampleSam I=${IN_FILE} O=${IN_FILE}_downsampled_${REDUCERATE}.bam P=${REDUCERATE} VALIDATION_STRINGENCY=SILENT TMP_DIR=/space1/tmp;
+	${JAVA} -Xmx32G -jar /hgsc_software/picard/picard-tools-2.18.4/picard.jar DownsampleSam I=${IN_FILE} O=${IN_FILE}_downsampled_${REDUCERATE}.bam P=${REDUCERATE} VALIDATION_STRINGENCY=SILENT TMP_DIR=/space1/tmp;
 
-	${JAVA} -Xmx32G -jar /hgsc_software/picard/picard-tools-2.6.0/picard.jar MarkDuplicates TMP_DIR=/space1/tmp AS=TRUE M=/dev/null VALIDATION_STRINGENCY=SILENT I=${IN_FILE}_downsampled_${REDUCERATE}.bam O=${IN_FILE}_downsampled_${REDUCERATE}.marked.bam
+	${JAVA} -Xmx32G -jar /hgsc_software/picard/picard-tools-2.18.4/picard.jar MarkDuplicates TMP_DIR=/space1/tmp AS=TRUE M=/dev/null VALIDATION_STRINGENCY=SILENT I=${IN_FILE}_downsampled_${REDUCERATE}.bam O=${IN_FILE}_downsampled_${REDUCERATE}.marked.bam
 
 	/hgsc_software/samtools/latest/samtools index ${IN_FILE}_downsampled_${REDUCERATE}.marked.bam
 fi;
@@ -40,7 +40,9 @@ if [ ${IN_FILE: -5} == ".cram" ]; then
 	echo "processing cram file!"
 	/hgsc_software/samtools/latest/samtools view -h -C -s ${REDUCERATE} ${IN_FILE} -o ${IN_FILE}_downsampled_${REDUCERATE}.cram;
 
-	/hgsc_software/java/jdk1.8.0_74/bin/java -Xmx32G -jar /stornext/snfs5/next-gen/scratch/phuang/software/picard-2.10.7/build/libs/picard.jar MarkDuplicates TMP_DIR=/space1/tmp AS=TRUE M=/dev/null VALIDATION_STRINGENCY=SILENT I=${IN_FILE}_downsampled_${REDUCERATE}.cram O=${IN_FILE}_downsampled_${REDUCERATE}.marked.bam R=${REFERENCE}
+	${JAVA} -Xmx32G -jar /stornext/snfs5/next-gen/scratch/phuang/software/picard-2.10.7/build/libs/picard.jar MarkDuplicates TMP_DIR=/space1/tmp AS=TRUE M=/dev/null VALIDATION_STRINGENCY=SILENT I=${IN_FILE}_downsampled_${REDUCERATE}.cram O=${IN_FILE}_downsampled_${REDUCERATE}.marked.bam R=${REFERENCE}
+
+	#${JAVA} -Xmx32G -jar /hgsc_software/picard/picard-tools-2.18.4/picard.jar MarkDuplicates TMP_DIR=/space1/tmp AS=TRUE M=/dev/null VALIDATION_STRINGENCY=SILENT I=${IN_FILE}_downsampled_${REDUCERATE}.cram O=${IN_FILE}_downsampled_${REDUCERATE}.marked.bam R=${REFERENCE}
 
 	/hgsc_software/samtools/latest/samtools index ${IN_FILE}_downsampled_${REDUCERATE}.marked.bam
 fi;
